4c4e0670cf2b0cae7607e7c43f41a582e35baedf
Devin Briscall <devinbriscall@gmail.com>
2022-02-25 11:41:06 -0500

    fgnb

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+GLOBAL.document = new JSDOM('https://metronometest.herokuapp.com/').window.document;
 const port = process.env.PORT || 3000;
 var app = express();
 app.listen(port, function () {
     console.log("server started successfully");
 })
 import Timer from "./timer.js";
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 
 const tempoDisplay = document.querySelector('.tempo');
